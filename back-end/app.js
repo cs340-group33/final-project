d13948773e3f716c5044eff86ae8bf8273244d7c
Paul <PaulNewling@gmail.com>
2020-05-17 11:33:49 -0700

    Heroku test2 (#3)
    * General backend setup, have not tested outside of quick postman checks
    
    * movie queries get,add,delete tested on postman
    
    * test
    
    * I'm not sure what changed, but it works on heroku now

--- back-end/app.js
+++ back-end/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const movieRouter = require('./routes/movies');
 
+
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const{body, check} = require('express-validator');
+
 const app = express();
 
-
-
+app.use(compression());
+app.use(helmet());
 
 // uses CORS on URL if isProduction is true.
 const isProduction = process.env.NODE_ENV === 'production';
@@ -18,22 +26,23 @@ const origin = {
 };
 app.use(cors(origin));
 
-
+// limits a client to 10 requests per minute.
 const limiter = rateLimit({
   windowMs: 1*60*1000,
   max: 25,
 });
 app.use(limiter);
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, '../dist')))
 
-
+app.use(cookieParser());
 
 app.use('/movies', movieRouter);
 app.use('*', indexRouter);
